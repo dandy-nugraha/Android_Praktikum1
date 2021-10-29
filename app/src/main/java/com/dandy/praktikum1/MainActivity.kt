9f12dc4e6153d2587ddb82b6974e88bf5e2566f0
dandydabx <dandyhendran@gmail.com>
2021-10-29 15:21:38 +0800

    Menambahkan OnClickListener dan Toast

--- app/src/main/java/com/dandy/praktikum1/MainActivity.kt
+++ app/src/main/java/com/dandy/praktikum1/MainActivity.kt
@@ -2,10 +2,20 @@ package com.dandy.praktikum1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.TextView
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val textView1  = findViewById<TextView>(R.id.Label1);
+
+        textView1.setOnClickListener(View.OnClickListener {
+            Toast.makeText(this,"Hai.. Saya Dandy", Toast.LENGTH_SHORT).show()
+        })
+
     }
 }
