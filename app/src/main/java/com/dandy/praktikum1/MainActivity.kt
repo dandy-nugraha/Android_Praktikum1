31392223910fd39a83590469e67e7fc38f59eb7a
dandydabx <dandyhendran@gmail.com>
2021-11-05 15:03:33 +0800

    menghubungkan Kotlin dengan komponen

--- app/src/main/java/com/dandy/praktikum1/MainActivity.kt
+++ app/src/main/java/com/dandy/praktikum1/MainActivity.kt
@@ -3,19 +3,29 @@ package com.dandy.praktikum1
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Button
+import android.widget.EditText
 import android.widget.TextView
 import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        override fun onCreate(savedInstanceState: Bundle?) {
+            super.onCreate(savedInstanceState)
+            setContentView(R.layout.activity_main)
+        val simpanButton = findViewById<Button>(R.id.simpanButton)
+        val batalButton = findViewById<Button>(R.id.batalButton)
 
-        val textView1  = findViewById<TextView>(R.id.Label1);
+        val namaEditText = findViewById<EditText>(R.id.namaEditText)
+        val emailEditText = findViewById<EditText>(R.id.emailEditText)
 
-        textView1.setOnClickListener(View.OnClickListener {
-            Toast.makeText(this,"Hai.. Saya Dandy..", Toast.LENGTH_SHORT).show()
-        })
+        simpanButton.setOnClickListener{
+            namaEditText.setText("")
+            emailEditText.setText("")
+        }
+
+        batalButton.setOnClickListener{
+            finish()
+        }
 
     }
 }
