132683a6cd5858daf870e94717c1e6dd1d404dfc
dandydabx <dandyhendran@gmail.com>
2021-11-19 16:03:02 +0800

    Splash Activity, PostDelayed dan menyembunyikan actionbar pada Splash Activity

--- app/src/main/java/com/dandy/praktikum1/SplashActivity.kt
+++ app/src/main/java/com/dandy/praktikum1/SplashActivity.kt
@@ -1,11 +1,20 @@
 package com.dandy.praktikum1
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+        getSupportActionBar()?.hide()
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }, 2000)
     }
 }
