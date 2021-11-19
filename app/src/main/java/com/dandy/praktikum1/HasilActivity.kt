40bde9bcefc9e176d7b2245226a719194bfe7612
dandydabx <dandyhendran@gmail.com>
2021-11-19 15:38:56 +0800

    Activity Hasil, Intent, Mengirim Data Menggunakan Intent

---
+++ app/src/main/java/com/dandy/praktikum1/HasilActivity.kt
@@ -0,0 +1,30 @@
+package com.dandy.praktikum1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
+
+class HasilActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_hasil)
+
+        val nama = intent.getStringExtra("nama")
+        val email = intent.getStringExtra("email")
+        val telp = intent.getStringExtra("telp")
+        val tgl = intent.getStringExtra("tgl")
+        val matkul = intent.getStringExtra("matkul")
+
+        val namaTextView = findViewById<TextView>(R.id.namaTextView)
+        val emailTextView = findViewById<TextView>(R.id.emailTextView)
+        val telpTextView = findViewById<TextView>(R.id.telpTextView)
+        val tglTextView = findViewById<TextView>(R.id.tglTextView)
+        val matkulTextView = findViewById<TextView>(R.id.matkulTextView)
+
+        namaTextView.setText(nama)
+        emailTextView.setText(email)
+        telpTextView.setText(telp)
+        tglTextView.setText(tgl)
+        matkulTextView.setText(matkul)
+    }
+}
--- app/src/main/java/com/dandy/praktikum1/MainActivity.kt
+++ app/src/main/java/com/dandy/praktikum1/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.dandy.praktikum1
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
@@ -22,12 +23,22 @@ class MainActivity : AppCompatActivity() {
         val matkulEditText = findViewById<EditText>(R.id.matkulEditText)
 
         simpanButton.setOnClickListener{
-            namaEditText.setText("")
-            emailEditText.setText("")
-            telpEditText.setText("")
-            tglEditText.setText("")
-            matkulEditText.setText("")
+            val nama = namaEditText.text.toString()
+            val email = emailEditText.text.toString()
+            val telp = telpEditText.text.toString()
+            val tgl = tglEditText.text.toString()
+            val jumlahMatkul = matkulEditText.text.toString()
             Toast.makeText(this, "Berhasil Simpan", Toast.LENGTH_LONG).show()
+
+        val intent = Intent(this,HasilActivity::class.java)
+            intent.putExtra("nama", nama)
+            intent.putExtra("email", email)
+            intent.putExtra("telp", telp)
+            intent.putExtra("tgl", tgl)
+            intent.putExtra("matkul", jumlahMatkul)
+
+            startActivity(intent)
+
         }
 
         batalButton.setOnClickListener{
